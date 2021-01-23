26405cdb495928f264cd3b9e7b5a5324df6f9f52
Anisha <agarwalanisha1111@gmail.com>
2021-01-23 14:37:45 +0530

    added script

--- app.js
+++ app.js
@@ -129,6 +129,6 @@ app.post('/upload', upload.single('image'), (req, res, next) => {
 
 app.use(authRoute);
 
-PORT = process.env.PORT;
+PORT = process.env.PORT||3000;
 
 app.listen(PORT, ()=> console.log(`Server started at port ${PORT}`))
